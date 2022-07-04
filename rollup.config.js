5f3fab9ba9d9f34bb7612bb2001468460f653b4f
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-07-04 16:07:24 +0600

    Modify Button and add Input component

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require("./package.json");
 
@@ -22,16 +23,17 @@ export default [
             },
         ],
         plugins: [
+            peerDepsExternal(),
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
-            postcss(),
+            terser(),
         ],
+        external: ["react", "react-dom", "styled-components"]
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
-        external: [/\.css$/],
     },
 ];
